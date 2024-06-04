04724a2a438aaa1c0a186ba7eebb157e5613ad41
Wukong Particle <wukong@particle.to>
2024-06-04 11:21:38 -0700

    nit

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -11,6 +11,7 @@ import {BlastManager} from "./libraries/BlastManager.sol";
 
 contract Airdrop is Ownable2Step, ReentrancyGuard, Multicall, BlastManager {
     /* Immutables */
+    // solhint-disable-next-line var-name-mixedcase
     address public TOKEN;
 
     /* Storage */
