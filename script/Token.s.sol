eb745802d0aa9b76acb74af6a083000c7cd891d1
Wukong Particle <wukong@particle.to>
2024-06-04 16:17:13 -0700

    token, airdrop, lockup deployer

---
+++ script/Token.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {ParticleToken} from "../contracts/Token.sol";
+
+address constant BLAST_POINT_CONTRACT = 0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800;
+address constant BLAST_POINT_ADMIN = 0x2d7Dc8B7Cc4Cc7E714034dc666E17577a0a28177;
+address constant MULTISIG = 0x7c8b9E2De6FfA465c6f717f349B3Ab13AB46481d;
+
+contract CounterScript is Script {
+    event Deployed(address token);
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        ParticleToken particleToken = new ParticleToken();
+        particleToken.setPointsOperator(BLAST_POINT_CONTRACT, BLAST_POINT_ADMIN);
+        particleToken.setManager(MULTISIG);
+
+        emit Deployed(address(particleToken));
+        vm.stopBroadcast();
+    }
+}
