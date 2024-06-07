6f9612de68248665e52733b8f655d9027790b0f6
Wukong Particle <wukong@particle.to>
2024-06-07 16:01:33 -0700

    set multisig as blast manager

--- script/Lockup.s.sol
+++ script/Lockup.s.sol
@@ -7,7 +7,7 @@ import {Timelock} from "../contracts/Timelock.sol";
 
 address constant BLAST_POINT_CONTRACT = 0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800;
 address constant BLAST_POINT_ADMIN = 0x2d7Dc8B7Cc4Cc7E714034dc666E17577a0a28177;
-address constant MULTISIG = 0x7c8b9E2De6FfA465c6f717f349B3Ab13AB46481d;
+address constant MULTISIG = 0xF60849FFe3CbF162d614D5f87bB5E20C074b5B91;
 address constant PARTICLE_TOKEN = address(0x42); // Placeholder
 uint256 constant MIN_DELAY = 3 days;
 
