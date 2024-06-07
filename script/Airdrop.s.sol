d1ab8614dcf29558827ce9c9f9377cc4ee24cfac
Wukong Particle <wukong@particle.to>
2024-06-07 16:10:03 -0700

    token deployed

--- script/Airdrop.s.sol
+++ script/Airdrop.s.sol
@@ -7,7 +7,7 @@ import {Airdrop} from "../contracts/Airdrop.sol";
 address constant BLAST_POINT_CONTRACT = 0x2536FE9ab3F511540F2f9e2eC2A805005C3Dd800;
 address constant BLAST_POINT_ADMIN = 0x2d7Dc8B7Cc4Cc7E714034dc666E17577a0a28177;
 address constant MULTISIG = 0xF60849FFe3CbF162d614D5f87bB5E20C074b5B91;
-address constant PARTICLE_TOKEN = address(0x42); // Placeholder
+address constant PARTICLE_TOKEN = 0xa027a3A04b44f79560153234E999b17C88e22DB9;
 bytes32 constant ROOT = bytes32(""); // Placeholder
 
 contract CounterScript is Script {
